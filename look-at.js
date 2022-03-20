6f06f5bbed6da755c6b02242164e4f549eaedf2a
devpreeti <65067758+devpreeti@users.noreply.github.com>
2022-03-20 13:00:35 +0530

    Add files via upload

---
+++ look-at.js
@@ -0,0 +1,10 @@
+/* global AFRAME */
+AFRAME.registerComponent('look-at', {
+    schema: { type: 'selector' },
+    
+    init: function () {},
+  
+    tick: function () {
+      this.el.object3D.lookAt(this.data.object3D.position)
+    }
+  })
